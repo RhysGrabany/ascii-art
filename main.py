8b9febcbd94345ec5cc5b1d09266b5f0ffbd6071
Rhys Grabany <r.grabany01@qub.ac.uk>
2019-06-06 18:55:46 +0100

    Finished doing the barebones of this project

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from PIL import Image
 import sys
 
 image = Image.open(sys.argv[1])
-rows, columns = image.size
-rgb_image = image.convert('RGB')
+image = image.transpose(Image.ROTATE_90)
+height, width = image.size
+
+new_width = 120
+new_height = 150
+
+img = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+rgb_image = img.convert('RGB')
 
 pixels = []
-for x in range(0, rows-1):
+for x in range(0, new_width):
     row = []
-    for y in range(0, columns-1):
+    for y in range(0, new_height):
         row.append(rgb_image.getpixel((x, y))) 
     pixels.append(row)
 
@@ -22,6 +29,21 @@ for x in range(0, len(pixels)):
         row.append(sum(pixel)/3)
     pixel_brightness.append(row)
 
-print(pixel_brightness)
+brightness_levels = ('`^",:;Il!i~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$')
 
+constant = 255/65
 
+pixel_ascii = []
+for x in range(0, len(pixel_brightness)):
+    row = []
+    for y in range(0, len(pixel_brightness[x])):
+        pixelb = pixel_brightness[x][y]
+        ascii_c = int(pixelb/constant)
+        #print(ascii_c)
+        for n in range(0, 3):
+            row.append(brightness_levels[ascii_c-1])
+    pixel_ascii.append(row)
+
+for x in range(0, len(pixel_ascii)):
+    print(*pixel_ascii[x], sep='')
+#print(*pixel_ascii, sep='')
